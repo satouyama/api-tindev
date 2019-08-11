e7329b4d59baff0366741057437d93a6440b961d
Douglas Lima Dias <senhorsatou@gmail.com>
2019-08-11 10:30:58 -0300

    initial

---
+++ src/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const routes = require('./routes');
+
+
+const port = process.env.Port || 3000;
+const mongoose = require('mongoose')
+const cors = require('cors');
+
+const httpServer = express();
+const server = require('http').Server(httpServer);
+const io = require('socket.io')(server);
+
+
+
+const connectedUsers = {}
+
+io.on('connection', (socket) =>{
+    const {user}  = socket.handshake.query;
+console.log(user, socket.id)
+
+connectedUsers[user] = socket.id;
+})
+
+mongoose.connect("mongodb+srv://tindev:tindev@cluster0-odmq7.mongodb.net/tindev?retryWrites=true&w=majority", {useNewUrlParser:true});
+
+
+httpServer.use((req, res, next) =>{
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+})
+
+httpServer.use(cors());
+httpServer.use(express.json())
+httpServer.use(routes);
+httpServer.use(express.static(path.join(__dirname, 'public')));
+
+// start servidor
+server.listen(port, ()=>{
+ console.log(`server listening on port ${port}`);
+})
