734fdefbe9f8b638b2f94a4550f69cc5715a5935
Douglas Lima Dias <senhorsatou@gmail.com>
2019-08-11 10:38:51 -0300

    initial

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const path = require('path')
 
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require('mongoose')
 const cors = require('cors');
 
