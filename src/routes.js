7204660a993a3bdad599e26ce972cd73bb6a5035
Douglas Lima Dias <senhorsatou@gmail.com>
2019-08-11 10:37:11 -0300

    initial

--- src/routes.js
+++ src/routes.js
@@ -9,5 +9,12 @@ routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store)
 routes.post('/devs/:devId/dislikes', DislikeController.store)
+routes.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+routes.get('/teste', (req, res) =>{
+    res.send('sucess')
+})
 
 module.exports = routes;
